ba52bb88a949203fad6dafdc04532758f6b53439
Adrian Jairus Valbuena <adrianwebpaypal@gmail.com>
2023-05-12 19:51:03 +0800

    login page start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./main/main.module').then(m => m.MainModule),
   },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  }
 ];
 
 @NgModule({
